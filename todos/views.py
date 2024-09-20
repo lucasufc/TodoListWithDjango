ce7d102b0f836364b2c849ef698e792e9dddfce4
lucas <lucashk.eng@gmail.com>
2024-09-20 14:22:57 -0300

    create todo list page

--- todos/views.py
+++ todos/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
 
-def home(request):
-    return render(request, "todos/home.html")
+from .models import Todo
+
+
+def todo_list(request):
+    todos = Todo.objects.all()
+    return render(request, "todos/todo_list.html", {"todos": todos})
