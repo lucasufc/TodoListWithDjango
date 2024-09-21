6a7e95ffa3a96025d20963133c64edf47b0130be
lucas <lucashk.eng@gmail.com>
2024-09-21 16:52:17 -0300

    add and edit tasks

--- setup/urls.py
+++ setup/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from todos.views import todo_list
+from todos.views import TodoCreateView, TodoListView, TodoUpdateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", todo_list),
+    path("", TodoListView.as_view(), name="todo_list"),
+    path("create", TodoCreateView.as_view(), name="todo_create"),
+    path("update/<int:pk>", TodoUpdateView.as_view(), name="todo_update"),
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
-
-
+from django.views.generic import ListView, CreateView, UpdateView
+from django.urls import reverse_lazy
 from .models import Todo
 
 
-def todo_list(request):
-    todos = Todo.objects.all()
-    return render(request, "todos/todo_list.html", {"todos": todos})
+class TodoListView(ListView):
+    model = Todo
+
+
+class TodoCreateView(CreateView):
+    model = Todo
+    fields = ["title", "deadline"]
+    success_url = reverse_lazy("todo_list")
+
+
+class TodoUpdateView(UpdateView):
+    model = Todo
+    fields = ["title", "deadline"]
+    success_url = reverse_lazy("todo_list")
